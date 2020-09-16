148f8bd745bcb08e6dd41887fa612ee7139b3644
Sebik Johnel <70603934+DalmatianuSebikk@users.noreply.github.com>
2020-09-16 16:41:02 +0300

    make new column visible

--- cos/admin.py
+++ cos/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Product
 
 class SeeItems(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'categorie', 'price')
+    list_display = ('id', 'name', 'description', 'categorie', 'price', 'explicatie')
     search_fields = ['name']
-    list_editable = ['name', 'description', 'categorie', 'price']
+    list_editable = ['name', 'description', 'categorie', 'price', 'explicatie']
 
 admin.site.register(Product, SeeItems)

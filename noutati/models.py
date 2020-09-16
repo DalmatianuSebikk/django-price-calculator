e45d1da90131324af4d3078971017df96701da91
Sebik Johnel <70603934+DalmatianuSebikk@users.noreply.github.com>
2020-09-16 13:49:28 +0300

    model issues

--- noutati/models.py
+++ noutati/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Articol(models.Model):
     title = models.CharField (max_length = 200)
-    date = models.DateField
-    description = models.TextField
+    date = models.DateField()
+    description = models.TextField()
     image = models.ImageField(blank = True)

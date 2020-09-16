98d7a9e7ca6f4a1bcda4c73b9b71fb4ec7b12afe
Sebik Johnel <70603934+DalmatianuSebikk@users.noreply.github.com>
2020-09-16 13:57:30 +0300

    datetime default added

--- noutati/models.py
+++ noutati/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
+import datetime
 class Articol(models.Model):
     title = models.CharField (max_length = 200)
-    date = models.DateField()
+    date = models.DateField(default = datetime.date.today)
     description = models.TextField()
     image = models.ImageField(blank = True)

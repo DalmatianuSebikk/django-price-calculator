6dcc4c0d3eae7bc58711a860df02f041ade0cddb
Sebik Johnel <70603934+DalmatianuSebikk@users.noreply.github.com>
2020-09-16 13:26:28 +0300

    admin stuff

--- noutati/admin.py
+++ noutati/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Articol
 # Register your models here.
+
+class SeeItems(admin.ModelAdmin):
+    list_display = ('id', 'title', 'date', 'description', 'image')
+    search_fields = ['title']
+
+admin.site.register(Articol)

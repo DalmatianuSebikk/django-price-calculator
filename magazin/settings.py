5cb88461f535e494000b9363b5f330b5bd59d891
Sebik Johnel <70603934+DalmatianuSebikk@users.noreply.github.com>
2020-09-22 17:57:01 +0300

    .env issue

--- magazin/settings.py
+++ magazin/settings.py
@@ -12,14 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import environ
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, True)
-)
-
-environ.Env.read_env()
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -29,10 +22,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['89.137.76.128', '0.0.0.0']
 
